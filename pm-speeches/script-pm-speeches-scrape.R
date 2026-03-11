2cfb5d0533993a698a97401fc4ba15e2b2a71e45
AyushBipinPatel <ayush.ap58@gmail.com>
2026-03-11 12:27:55 +0530

    script, output, data for abv speeches

--- pm-speeches/script-pm-speeches-scrape.R
+++ pm-speeches/script-pm-speeches-scrape.R
@@ -10,6 +10,7 @@ library(tidyverse)
 ## raw speech text -mms
 
 mms_vol3 <- pdftools::pdf_text(pdf = "pm-speeches/selectedspeeches03unse.pdf") 
+abv_vol1 <- pdftools::pdf_text(pdf = "pm-speeches/primeministerata01unse.pdf")
 
 ## speech and title extraction - mms
 
@@ -23,6 +24,8 @@ get_all_speechs <- function(title, pg, last_pg, data) {
   
   
 }
+
+### MMS
 
 tibble(
     content = mms_vol3[7:20] |>
@@ -58,3 +61,44 @@ pmap(
 
 
 write.csv(drmms_speech_text_df_3,"pm-speeches/DrManmohan-singh-speeches.csv")
+
+
+### ABV
+
+tibble(
+    content = abv_vol1[5:13] |>
+                read_lines() |>
+                str_squish()
+) |>
+  separate(col = content,
+    sep = "(?<=[[:upper:]]) (?=[[:digit:]]{1,3}$)",
+    into = c("title","pg")) |> 
+  filter(title != "") |> 
+  fill(pg,.direction =  "down") |> 
+  filter(!is.na(pg)) |>
+  group_by(pg) |>
+  summarise(
+    title = str_c(title, collapse = " ")
+  ) |>
+  ungroup() |>
+  arrange(as.numeric(pg)) |> 
+  mutate(
+    last_pg = lead(as.integer(pg)) -1
+  ) |> 
+  filter(!is.na(last_pg))|> 
+  filter(title != "INDEX") -> pmabv_speeches_vol1
+
+pmap(
+    pmabv_speeches_vol1 |>
+      relocate(title,pg,last_pg) |>
+      mutate(
+        pg =as.numeric(pg) +14,
+        last_pg = last_pg+14
+      ),
+    get_all_speechs,data = abv_vol1
+) |>
+  list_rbind() -> abv_speech_text_df_3
+
+
+
+write.csv(abv_speech_text_df_3,"pm-speeches/AtalBihari-Vajpayee-speeches.csv")
